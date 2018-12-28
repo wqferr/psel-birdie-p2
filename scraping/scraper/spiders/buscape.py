abbbb1e7d421d72b01f1c5d85915024327456682
wqferr <wqferr@gmail.com>
2018-12-28 21:45:05 -0200

    Add SOURCE and CATEGORY as columns

--- scraping/scraper/spiders/buscape.py
+++ scraping/scraper/spiders/buscape.py
@@ -3,26 +3,51 @@ from scrapy.http import Request
 from urllib.parse import urljoin
 
 
+def subselect(subquery):
+    def do_subselect(partial_results):
+        return partial_results.xpath(subquery)
+    return do_subselect
+
+
 class BuscapeSpider(scrapy.Spider):
     name = 'buscape'
     allowed_domains = ['www.buscape.com.br']
     start_urls = ['https://www.buscape.com.br/']
 
-    _title_selector = '//div[contains(@class, "card--product__name")]/text()'
+    _product_title_selector = '//div[contains(@class, "card--product__name")]/text()'
+    _product_href_selector = '//div[contains(@class, "card--product__name")]/../@href'
 
     def __init__(self, n_pages=20, category=''):
         self.n_pages = int(n_pages)
         self.category = category
+        self.base_url = None
 
     def parse(self, response):
-        base_url = urljoin(response.url, self.category)
+        self.base_url = urljoin(response.url, self.category)
 
         for i in range(0, self.n_pages):
             yield Request(
-                f'{base_url}?pagina={i + 1}',
+                f'{self.base_url}?pagina={i + 1}',
                 callback=self.parse_page
             )
 
     def parse_page(self, response):
-        titles = response.xpath(self._title_selector).extract()
-        return ({'TITLE': title} for title in titles)
+        titles = response.xpath(self._product_title_selector).extract()
+        sources = response.xpath(self._product_href_selector).extract()
+        sources = map(
+            lambda s: urljoin(self.base_url, s),
+            sources
+        )
+        return (
+            {
+                'TITLE': title,
+                'SOURCE': source,
+                'CATEGORY': self.category
+            } for title, source in zip(titles, sources))
+        # titles = response.xpath(self._title_selector).extract()
+        # return (
+        #     {
+        #         'TITLE': title,
+        #         'SOURCE': response.url,
+        #         'CATEGORY': self.category
+        #     } for title in titles)
